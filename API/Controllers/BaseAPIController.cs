c6f890e5d04376a41fec70c22767e9dd3afcde30
DuyDoan190203 <duydoan190203@gmail.com>
2023-10-09 22:34:12 +0300

    CRUD application using the CQRS + Mediator pattern

--- API/Controllers/BaseAPIController.cs
+++ API/Controllers/BaseAPIController.cs
@@ -1,4 +1,7 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+
+//DRY Principle in Programming
 
 namespace API.Controllers
 {
@@ -6,6 +9,10 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseAPIController : ControllerBase
     {
+       private IMediator _mediator;
+
+       protected IMediator Mediator => _mediator ??= 
+            HttpContext.RequestServices.GetService<IMediator>();
         
     }
 }
