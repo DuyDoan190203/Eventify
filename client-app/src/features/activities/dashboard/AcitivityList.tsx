72c464c7b7f0b84adea71cf5e1774eb23d49c9ef
DuyDoan190203 <duydoan190203@gmail.com>
2023-10-11 01:28:32 +0300

    CRUD application for React(client) side

---
+++ client-app/src/features/activities/dashboard/AcitivityList.tsx
@@ -0,0 +1,34 @@
+import { Button, Item, ItemGroup, ItemHeader, ItemMeta, Label, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/Activity"
+
+interface Props{
+  activities: Activity[];
+  selectActivity: (id: string) => void;
+  deleteActivity: (id: string) => void;
+}
+
+export default function ActivityList({activities, selectActivity, deleteActivity}: Props) {
+  return(
+    <Segment>
+      <ItemGroup divided>
+          {activities.map(activity => (
+             <Item key={activity.id}>
+                <Item.Content>
+                  <ItemHeader as='a'>{activity.title}</ItemHeader>
+                  <ItemMeta>{activity.date}</ItemMeta>
+                  <Item.Description>
+                    <div>{activity.description}</div>
+                    <div>{activity.city}, {activity.venue}</div>
+                  </Item.Description>
+                  <Item.Extra>
+                    <Button onClick={() => selectActivity(activity.id)} floated="right" content='View' color="blue"/>
+                    <Button onClick={() => deleteActivity(activity.id)} floated="right" content='Delete' color="red"/>
+                    <Label basic content ={ activity.category}/>
+                  </Item.Extra>
+                </Item.Content>
+             </Item>
+          ))}
+      </ItemGroup>
+    </Segment>
+  )
+}
