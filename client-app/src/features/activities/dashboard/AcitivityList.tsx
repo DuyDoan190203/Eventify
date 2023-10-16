a0c9bdc9c04815117e7b01232e06467d8d375cd0
DuyDoan190203 <duydoan190203@gmail.com>
2023-10-16 16:04:13 +0300

    Using Mobx to centralize the State and make the code cleaner.

--- client-app/src/features/activities/dashboard/AcitivityList.tsx
+++ client-app/src/features/activities/dashboard/AcitivityList.tsx
@@ -1,26 +1,23 @@
 import { Button, Item, ItemGroup, ItemHeader, ItemMeta, Label, Segment } from "semantic-ui-react";
-import { Activity } from "../../../app/models/Activity"
 import { SyntheticEvent, useState } from "react";
+import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-interface Props{
-  activities: Activity[];
-  selectActivity: (id: string) => void;
-  deleteActivity: (id: string) => void;
-  submitting: boolean;
-}
+export default observer(function ActivityList() {
+  const {activityStore} = useStore();
+  const {deleteActivity, activitiesByDate, loading} = activityStore;
 
-export default function ActivityList({activities, selectActivity, deleteActivity, submitting}: Props) {
+
   const [target, setTarget] = useState('');
 
   function handleDeletedActivity(e: SyntheticEvent<HTMLButtonElement>, id: string) {
     setTarget(e.currentTarget.name);
     deleteActivity(id);
   }
-
   return(
     <Segment>
       <ItemGroup divided>
-          {activities.map(activity => (
+          {activitiesByDate.map(activity => (
              <Item key={activity.id}>
                 <Item.Content>
                   <ItemHeader as='a'>{activity.title}</ItemHeader>
@@ -30,10 +27,10 @@ export default function ActivityList({activities, selectActivity, deleteActivity
                     <div>{activity.city}, {activity.venue}</div>
                   </Item.Description>
                   <Item.Extra>
-                    <Button onClick={() => selectActivity(activity.id)} floated="right" content='View' color="blue"/>
+                    <Button onClick={() => activityStore.selectActivity(activity.id)} floated="right" content='View' color="blue"/>
                     <Button
                       name={activity.id} 
-                      loading={submitting && target === activity.id} 
+                      loading={loading && target === activity.id} 
                       onClick={(e) => handleDeletedActivity(e, activity.id)} 
                       floated="right" 
                       content='Delete' 
@@ -46,4 +43,4 @@ export default function ActivityList({activities, selectActivity, deleteActivity
       </ItemGroup>
     </Segment>
   )
-}
+})
--- client-app/src/features/activities/details/ActivitiyDetails.tsx
+++ client-app/src/features/activities/details/ActivitiyDetails.tsx
@@ -1,16 +1,17 @@
 import { Button, Card, Image } from "semantic-ui-react";
-import { Activity } from "../../../app/models/Activity";
+import { useStore } from "../../../app/stores/store";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
-interface Props{
-  activity: Activity;
-  cancelSelectActivity: () => void;
-  openForm: (id: string) => void; 
-}
+export default function AcitivityDetails() {
+  const {activityStore} = useStore();// Use Mobx to Centralize state
+  const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+  //Destructuring the Props
 
-export default function AcitivityDetails({activity, cancelSelectActivity, openForm}: Props) {
+
+  if(!activity) return <LoadingComponent/>;
   return (
     <Card fluid>
-      <Image src={`/assets/categoryImages/${activity.category}.jpg`}/>
+      <Image src={`/assets/categoryImages/${activity!.category}.jpg`}/>
       <Card.Content>
         <Card.Header>{activity.title}</Card.Header>
         <Card.Meta>
@@ -23,7 +24,7 @@ export default function AcitivityDetails({activity, cancelSelectActivity, openFo
           <Card.Content extra>
             <Button.Group widths='2'>
               <Button onClick={() => openForm(activity.id)}basic color="blue" content='Edit'/>
-              <Button onClick={cancelSelectActivity} basic color="grey" content='Cancel'/>
+              <Button onClick={cancelSelectedActivity} basic color="grey" content='Cancel'/>
             </Button.Group>
           </Card.Content>
   </Card>
